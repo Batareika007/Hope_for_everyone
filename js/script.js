39eebead4c3b7a8144992984d6e1898848a1745c
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-02-10 19:52:05 +0300

    removed preloader
    I will fix it later

--- js/script.js
+++ js/script.js
@@ -1,9 +1,2 @@
 $("#header").load("../assets/header.html");
 $("#footer").load("../assets/footer.html");
-
-//paste this code under head tag or in a seperate js file.
-// Wait for window load
-$(this).load(function () {
-    // Animate loader off screen
-    $(".se-pre-con").fadeOut("slow");;
-});
