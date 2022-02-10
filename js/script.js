2e33d6dc42c332bf61430cf2e330d0ccf03a888b
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-02-10 19:15:52 +0300

    testing bug fix3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-$( "#header" ).load( "../assets/header.html" );
-$( "#footer" ).load( "../assets/footer.html" );
+$("#header").load("../assets/header.html");
+$("#footer").load("../assets/footer.html");
 
+//paste this code under head tag or in a seperate js file.
+// Wait for window load
+$(window).load(function () {
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");;
+});
