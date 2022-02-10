e03ecb673d16a8cb9196c1d5229360216b4238c3
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-02-10 19:37:15 +0300

    testing 2

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $("#footer").load("../assets/footer.html");
 
 //paste this code under head tag or in a seperate js file.
 // Wait for window load
-$(window).load(function () {
+$(this).load(function () {
     // Animate loader off screen
     $(".se-pre-con").fadeOut("slow");;
 });
