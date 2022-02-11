61c06a388b13921de751d8d258a5162fa337c04b
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-02-11 18:54:45 +0300

    configure header links
    need to check how run scripts correctly

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 $(function() {
     $("#header").load("assets/header.html");
     $("#footer").load("assets/footer.html");
+    
 })
+
+$('.dropdown__button').click(function(){
+    $('.dropdown__list').toggle('.active');
+});
+$('.leng').click(function(){
+    $('.dropdown__list').toggle('.active');
+});
